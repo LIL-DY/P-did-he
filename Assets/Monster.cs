5db9c6b15319be14608380eabc174749ca52178a
Zhensinho <151174033+Zhensinho@users.noreply.github.com>
2025-01-13 11:02:17 +0100

    Monster

---
+++ Assets/Monster.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+using UnityEngine;
+
+public class MonsterAI : MonoBehaviour
+{
+    public Transform player; //Referens till spelarens transform
+    public float speed = 2f; //Hastigheten på monstret
+    public float stopDistance = 1f; //Avstånd vid vilket monstret stannar
+    public float detectionRadius = 5f; //Radius inom vilket monstret upptäcker spelaren
+    public int hunt = 0;
+    private void Update()
+    {
+
+        {
+            if (player == null)
+            {
+
+                Debug.LogWarning("Player is not assigned to the MonsterAI script");
+                return;
+            }
+
+            // Beräkna avståndet mellan mosntret och spelaren
+            float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+            //Om avståendet är större än stopDistance, rör monstret mot spelaren
+            if (distanceToPlayer <= detectionRadius)
+            {
+                hunt = 1;
+                if (hunt >= 1)
+                {
+                    if (distanceToPlayer > stopDistance)
+                    {
+
+                        Vector2 direction = (player.position - transform.position).normalized;
+                        transform.position += (Vector3)direction * speed * Time.deltaTime;
+                    }
+                }
+               
+            }
+        }   
+            
+            
+                
+            
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
